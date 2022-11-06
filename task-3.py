88537a9ca89aeb6c7a6e9e4cbf57a75852a27643
yas <ysv411@gmail.com>
2022-11-07 01:29:41 +0200

    5

--- task-3.py
+++ task-3.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 
 # Write a utility for generating passwords according to a given template that supports the CLI interface
@@ -31,9 +32,7 @@ import argparse
 
 # Output: supports pipe redirect
 
-#args = parser.parse_args()
-#print(args.filename, args.count, args.verbose)
-
+"""
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
 parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum, default=max,
@@ -42,3 +41,40 @@ parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum,
 args = parser.parse_args()
 print(args.accumulate(args.integers))
 
+print(sys.argv)	# prints list of arguments passed to scripts
+				# [0] = name of script, [1] - first argument, etc
+				# args = type of Str, numbers must be converted to Int
+
+a=sys.argv[1]
+print(a)
+b=sys.argv[2]
+print(b)
+"""
+
+ps = argparse.ArgumentParser()
+# ps.add_argument("test")
+
+# Positional argument
+ps.add_argument("a", type=int, help='First argument')
+ps.add_argument("b", type=int, help='Second argument')
+
+# Optional(named) argument
+ps.add_argument("-a", "--action", help='What to do with numbers', required=True, default='summa')
+# action='store_const', const='10'
+# action='store_true' | 'store_false'
+# nargs=2		# = number of args
+# choises=['1', '2', '3']
+
+args = ps.parse_args()
+print(args)
+
+def summa(a,b):
+	print(a+b)
+
+def minus(a,b):
+	print(a-b)
+
+if args.action == 'summa':
+	summa(args.a, args.b)
+elif args.action == 'minus':
+	minus(args.a, args.b)
